9d66630c7d5b61efe5cee89bf1dd36a968f33960
Suman <sumandas251@gmail.com>
2023-01-01 21:41:20 +0530

    Basic of Parcel bundler

---
+++ Practical/Class-02/App.js
@@ -0,0 +1,31 @@
+/**
+ * In this class we learnt about crating React element which are tags in html
+ * We get to know about Parcel - Which is a bundler
+ * This parcel bundler is really powerfull and empowers React to to do the following -
+ *
+ * 1. HMR - Hot Module Replacement - (If we change and save our file it will automatically reload the client by using)
+ *      File watcher algorithm - Parcel use this algorithm which was ariten in C++
+ * 2. Bundling
+ * 3. MINIFY
+ * 4. Cleaning our code
+ * 5. dev and production build - Super fast build algorithm
+ * 6. Image optimization
+ * 7. Caching while development - thats why after first build the later build take very less time
+ * 8.
+ */
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading = React.createElement(
+  "h1",
+  {},
+  "Namaste React class 02 practice"
+);
+
+const heading2 = React.createElement("h2", { key: 1 }, "This is a h2 tag");
+
+const container = React.createElement("div", { key: 2 }, [heading, heading2]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(container);
