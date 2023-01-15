36352b4615311a18be188fa0a7ce689552e48973
Suman <sumandas251@gmail.com>
2023-01-16 01:20:00 +0530

    Making search bar work

--- Practical/Class-05/src/components/Body.js
+++ Practical/Class-05/src/components/Body.js
@@ -1,13 +1,45 @@
 import { restaurantList } from "../config";
 import RestaurantCard from "./RestaurantCard";
-import Search from "./Search";
+import { useState } from "react";
+
+function searchRestaurant(searchInput, restaurants) {
+  console.log("check " + restaurants);
+  return restaurants.filter((item) => item?.data?.name.includes(searchInput));
+}
 
 const Body = () => {
+  let [restaurants, setRestaurants] = useState(restaurantList);
+  let [searchInput, setSearchInput] = useState("");
   return (
     <>
-      <Search />
+      <div className="search-container">
+        <input
+          type="text"
+          className="search-input"
+          placeholder="search here"
+          value={searchInput}
+          onChange={(e) => {
+            // searchTxt = e.target.value;
+            setSearchInput(e.target.value);
+            // console.log(searchInput);
+          }}
+        />
+        <button
+          onClick={() => {
+            const data = searchRestaurant(searchInput, restaurants);
+            setRestaurants(data);
+            // if (data.length > 0) {
+            //   setRestaurants(data);
+            // } else {
+            //   setRestaurants({});
+            // }
+          }}
+        >
+          Search - {searchInput}
+        </button>
+      </div>
       <div className="restaurant-list">
-        {restaurantList.map((restaurant) => {
+        {restaurants.map((restaurant) => {
           return (
             <RestaurantCard {...restaurant.data} key={restaurant.data.id} />
           );
--- Practical/Class-05/src/components/Search.js
+++ Practical/Class-05/src/components/Search.js
@@ -1,25 +1,50 @@
 import { useState } from "react";
+import restaurantList from "../config";
+
+function searchRestaurant(searchInput, restaurants) {
+  // console.log("check " + restaurants);
+  return restaurants.filter((item) => item?.data?.name.includes(searchInput));
+}
 
 const Search = () => {
   // this hook is used to create local state variable
   // first parameter is the variable and second one is the setter function to chnage the variable value
-  let [searchTxt, setSearchTxt] = useState();
+  let [restaurants, setRestaurants] = useState(restaurantList);
+  let [searchInput, setSearchInput] = useState("");
+
+  // creating toggle
+  // let [searchClick, setSearchClick] = useState("false");
   return (
     <div className="search-container">
       <input
         type="text"
-        className="search-inout"
+        className="search-input"
         placeholder="search here"
-        value={searchTxt}
+        value={searchInput}
         onChange={(e) => {
           // searchTxt = e.target.value;
-          setSearchTxt(e.target.value);
-          console.log(searchTxt);
+          setSearchInput(e.target.value);
+          // console.log(searchInput);
         }}
       />
-      <button>Search</button>
+      <button
+        onClick={() => {
+          const data = searchRestaurant(searchInput, restaurants);
+          setRestaurants(data);
+        }}
+      >
+        Search - {searchInput}
+      </button>
     </div>
   );
 };
 
 export default Search;
+
+/**
+ * The search Component should not be here as we are trying to modify the restaurants based on
+ * the user search options. So ideally I need to put the search box inside the body component.
+ *
+ * Its working as expected now and exactly the same way i learnt in class. But need to try this -
+ * Whether I can pass state of one component to another component.
+ */
